0e904becc9d60d35bc09a6c62c93c5a3a355f26d
Mohd Imran <mohd-i001@webreinvent.com>
2024-03-07 13:05:48 +0530

    changed minSdkVersion to 21

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:vaahflutter/app_config.dart';
-
-void main() {
-  testWidgets('test', (WidgetTester tester) async {
-    await tester.pumpWidget(const AppConfig());
-    expect(find.byType(AppConfig), findsOneWidget);
-  });
-}
